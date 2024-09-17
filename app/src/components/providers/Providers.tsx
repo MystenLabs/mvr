64b71efd66a2c86235d5e3a3b87a006395a0d253
Manolis Liolios <manos@mystenlabs.com>
2024-09-17 17:44:40 +0300

    Fix missing styles for dapp-kit

--- app/src/components/providers/Providers.tsx
+++ app/src/components/providers/Providers.tsx
@@ -4,19 +4,15 @@ import { type ReactNode } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "next-themes";
 import { getFullnodeUrl } from "@mysten/sui/client";
-import { SuiClientProvider } from "@mysten/dapp-kit";
-import dynamic from "next/dynamic";
+import { SuiClientProvider, WalletProvider } from "@mysten/dapp-kit";
+
+import "@mysten/dapp-kit/dist/index.css";
 
 const networks = {
   devnet: { url: getFullnodeUrl("devnet") },
   testnet: { url: getFullnodeUrl("testnet") },
   mainnet: { url: getFullnodeUrl("mainnet") },
 };
-
-const WalletProvider = dynamic(() => import('@mysten/dapp-kit').then((p) => p.WalletProvider), {
-  ssr: false,
-});
-
 
 const client = new QueryClient();
 
@@ -26,7 +22,12 @@ export default function Providers({ children }: { children: ReactNode }) {
       <ThemeProvider attribute="class" defaultTheme="dark">
         <QueryClientProvider client={client}>
           <SuiClientProvider networks={networks} defaultNetwork="mainnet">
-            <WalletProvider autoConnect>{children}</WalletProvider>
+            <WalletProvider
+              stashedWallet={{ name: "Move Registry (mvr)" }}
+              autoConnect
+            >
+              {children}
+            </WalletProvider>
           </SuiClientProvider>
         </QueryClientProvider>
       </ThemeProvider>
