40388c1ac43e19568d15127f03b1424696bcc113
Manolis Liolios <manos@mystenlabs.com>
2024-09-30 23:44:31 +0300

    Hygiene + co

---
+++ app/src/app/faq/page.tsx
@@ -0,0 +1,10 @@
+import { PlainPageLayout } from "@/components/layouts/PlainPageLayout";
+import { Text } from "@/components/ui/Text";
+
+export default function Faq() {
+  return (
+    <PlainPageLayout>
+      <Text variant="heading/bold">FAQ goes here!</Text>
+    </PlainPageLayout>
+  );
+}
