78a692823b0e3825490759ed8414a62b401b566f
Manolis Liolios <manos@mystenlabs.com>
2024-10-07 12:49:53 +0300

    Fix redirect, and remove edit NFT for now

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,18 +1,8 @@
 'use client';
 
-import { useRouter } from "next/navigation";
+import { redirect } from 'next/navigation';
 import { useEffect } from "react";
 
 export default function HomePage() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.push("/apps");
-  }, []);
-
-  return (
-    <main className="container">
-      <h1>This is the Move Registry homepage and I don't think you can land here anyhow!</h1>
-    </main>
-  );
+  redirect('/apps');
 }
