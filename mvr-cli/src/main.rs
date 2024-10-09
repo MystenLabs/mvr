207664bb6aed7e4134f189e834b4ed2818dff9e2
Rijnard van Tonder <rvantonder@gmail.com>
2024-10-08 21:56:34 -0700

    modify CLI command parsing

--- mvr-cli/src/main.rs
+++ mvr-cli/src/main.rs
@@ -11,7 +11,7 @@ struct Cli {
     resolve_move_dependencies: Option<String>,
 
     #[command(subcommand)]
-    command: CliCommand,
+    command: Option<CliCommand>,
 
     /// Output the result in JSON format
     #[arg(long, global = true)]
@@ -26,13 +26,16 @@ async fn main() -> Result<()> {
         check_sui_version(MINIMUM_BUILD_SUI_VERSION);
         // Resolver function that `sui move build` expects to call.
         resolve_move_dependencies(value).await?;
-    } else {
-        let output = cli.command.execute().await?;
+    } else if let Some(command) = cli.command {
+        let output = command.execute().await?;
         if cli.json {
             println!("{}", serde_json::to_string_pretty(&output)?);
         } else {
             println!("{}", output);
         }
+    } else {
+        // No subcommand or resolve_move_dependencies flag provided
+        clap::Command::new("mvr").print_help()?;
     }
     Ok(())
 }
