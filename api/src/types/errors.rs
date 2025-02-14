f8f9ce04854b4afd8886376e3a335c715c7c71aa
Manolis Liolios <manos@mystenlabs.com>
2025-02-14 17:07:21 +0200

    Remove non-needed errors & map properly

--- api/src/types/errors.rs
+++ api/src/types/errors.rs
@@ -38,38 +38,24 @@ impl IntoResponse for ApiError {
 
 impl From<MoveRegistryError> for ApiError {
     fn from(error: MoveRegistryError) -> Self {
-        ApiError::InternalServerError(error.to_string())
+        ApiError::BadRequest(error.to_string())
     }
 }
 
 impl From<NameServiceError> for ApiError {
     fn from(error: NameServiceError) -> Self {
-        ApiError::InternalServerError(error.to_string())
+        ApiError::BadRequest(error.to_string())
     }
 }
 
 #[derive(thiserror::Error, Debug, Serialize, Deserialize, Clone, Eq, PartialEq)]
 pub enum MoveRegistryError {
-    // The chain identifier is not available, so we cannot determine where to look for the name.
-    #[error("Move Registry: Cannot determine which chain to query due to an internal error.")]
-    ChainIdentifierUnavailable,
     // The name was found in the service, but it is not a valid name.
     #[error("Move Registry: The request name {0} is malformed.")]
     InvalidName(String),
 
     #[error("Move Registry: The request type {0} is malformed.")]
     InvalidType(String),
-
-    #[error("Move Registry: External API url is not available so resolution is not on this RPC.")]
-    ExternalApiUrlUnavailable,
-
-    #[error(
-        "Move Registry: Internal Error, failed to query external API due to an internal error: {0}"
-    )]
-    FailedToQueryExternalApi(String),
-
-    #[error("Move Registry Internal Error: Failed to parse external API's response: {0}")]
-    FailedToParseExternalResponse(String),
 
     #[error("Move Registry: The name {0} was not found.")]
     NameNotFound(String),
