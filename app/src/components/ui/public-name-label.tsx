89de7352c81864eb066e26999fc9874299f860c5
Manolis Liolios <manos@mystenlabs.com>
2025-04-08 20:19:05 +0300

    nit

--- app/src/components/ui/public-name-label.tsx
+++ app/src/components/ui/public-name-label.tsx
@@ -1,6 +1,6 @@
 export function PublicNameLabel() {
   return (
-    <span className="py-2xs px-xs bg-bg-accentBleedthrough1 ml-2 rounded-full bg-positive text-[10px] text-content-primary">
+    <span className="py-2xs px-xs bg-bg-accentBleedthrough1 ml-xs rounded-full bg-positive text-[10px] text-content-primary">
       Public Name
     </span>
   );
