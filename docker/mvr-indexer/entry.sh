ba12403847cafe4457b3bd5ca7d4d97f2134aa9c
Tom Harbert <tom@mystenlabs.com>
2025-04-09 09:07:48 -0700

    add postgres pkg (#177)

--- docker/mvr-indexer/entry.sh
+++ docker/mvr-indexer/entry.sh
@@ -3,4 +3,7 @@
 export RUST_BACKTRACE=1
 export RUST_LOG=debug
 
+# for initdb
+export PATH="/usr/lib/postgresql/$(ls /usr/lib/postgresql | head -n 1)/bin:$PATH"
+
 /opt/mysten/bin/mvr-indexer --database-url "$DB_URL" --env "$NETWORK"
