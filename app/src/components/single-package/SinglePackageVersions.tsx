d9917dd38dd2c903636e510df15f8987e3dadb49
Manolis Liolios <manos@mystenlabs.com>
2025-04-17 13:41:39 +0300

    Fix fonts etc

--- app/src/components/single-package/SinglePackageVersions.tsx
+++ app/src/components/single-package/SinglePackageVersions.tsx
@@ -5,6 +5,7 @@ import { beautifySuiAddress } from "@/lib/utils";
 import { Text } from "../ui/Text";
 import { CopyBtn } from "../ui/CopyBtn";
 import LoadingState from "../LoadingState";
+import { DependentsCountLabel } from "./SinglePackageTabs";
 
 export function SinglePackageVersions({ name }: { name: ResolvedName }) {
   const network = usePackagesNetwork() as "mainnet" | "testnet";
@@ -20,44 +21,62 @@ export function SinglePackageVersions({ name }: { name: ResolvedName }) {
   }
 
   return (
-    <table className="w-full">
-      <thead>
-        <tr className="border-b border-stroke-secondary text-left">
-          <th className="px-md py-sm">Version</th>
-          <th className="px-md py-sm">Address</th>
-        </tr>
-      </thead>
-      <tbody>
-        {versions?.map((version) => (
-          <tr
-            key={version.version}
-            className="border-b border-stroke-secondary"
-          >
-            <td className="px-md py-sm">
+    <>
+      <Text
+        as="div"
+        kind="heading"
+        size="heading-regular"
+        className="flex items-center gap-sm"
+      >
+        Versions <DependentsCountLabel count={name.version} hasMore={false} />
+      </Text>
+      <table className="w-full">
+        <thead>
+          <tr className="border-b border-stroke-secondary text-left">
+            <th className="py-sm">
               <Text as="span" size="label-small" kind="label">
-                {version.version}
+                Version
               </Text>
-
-              {version.version === name.version && (
-                <Text
-                  as="span"
-                  size="label-xs"
-                  kind="label"
-                  className="ml-md rounded-md bg-bg-accentBleedthrough3 px-sm py-xs"
-                >
-                  Latest
-                </Text>
-              )}
-            </td>
-            <td className="flex items-center gap-sm px-md py-sm">
-              <Text as="div" size="label-small" kind="label">
-                {beautifySuiAddress(version.address, 10)}
+            </th>
+            <th className="py-sm">
+              <Text as="span" size="label-small" kind="label">
+                Address
               </Text>
-              <CopyBtn text={version.address} />
-            </td>
+            </th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {versions?.map((version) => (
+            <tr
+              key={version.version}
+              className="border-b border-stroke-secondary text-content-secondary"
+            >
+              <td className="py-sm">
+                <Text as="span" size="label-small" kind="label">
+                  {version.version}
+                </Text>
+
+                {version.version === name.version && (
+                  <Text
+                    as="span"
+                    size="label-xs"
+                    kind="label"
+                    className="ml-md rounded-md bg-bg-accentBleedthrough3 px-sm py-xs"
+                  >
+                    Latest
+                  </Text>
+                )}
+              </td>
+              <td className="flex items-center gap-sm py-sm">
+                <Text as="div" size="label-small" kind="label">
+                  {beautifySuiAddress(version.address, 10)}
+                </Text>
+                <CopyBtn text={version.address} />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 }
