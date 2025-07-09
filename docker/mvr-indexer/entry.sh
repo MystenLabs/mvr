7519c67d020abf51458d63debc87400193e1149f
patrick <patrickkuo@me.com>
2025-07-09 11:58:27 +0100

    enable tokio console

--- docker/mvr-indexer/entry.sh
+++ docker/mvr-indexer/entry.sh
@@ -5,5 +5,7 @@ export RUST_LOG=debug
 
 # for initdb
 export PATH="/usr/lib/postgresql/$(ls /usr/lib/postgresql | head -n 1)/bin:$PATH"
+export RUSTFLAGS="--cfg tokio_unstable"
+export TOKIO_CONSOLE=1
 
 /opt/mysten/bin/mvr-indexer --database-url "$DB_URL" --env "$NETWORK"
