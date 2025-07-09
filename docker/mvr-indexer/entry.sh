b65c4b94da32a4b88298f1ccd46db8e1405b673e
patrick <patrickkuo@me.com>
2025-07-09 14:11:55 +0100

    minor fix

--- docker/mvr-indexer/entry.sh
+++ docker/mvr-indexer/entry.sh
@@ -7,5 +7,6 @@ export RUST_LOG=debug
 export PATH="/usr/lib/postgresql/$(ls /usr/lib/postgresql | head -n 1)/bin:$PATH"
 export RUSTFLAGS="--cfg tokio_unstable"
 export TOKIO_CONSOLE=1
+export TOKIO_CONSOLE_BIND="0.0.0.0:6669"
 
 /opt/mysten/bin/mvr-indexer --database-url "$DB_URL" --env "$NETWORK"
